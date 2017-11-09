1caca52077bdf5b6dd6ecd6e14741fe4c09864eb
ovdien <Nickolay.Ovdienko@emc.com>
2017-11-09 17:13:41 +0300

    update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,5 @@
+__all__ = ['GenericSlotsObjectModel', 'GenericDictObjectModel', 'PrettyDictObjectModel']
+
 from slots_object_model import GenericSlotsObjectModel
 from dict_object_model import GenericDictObjectModel
+from dict_object_model import PrettyDictObjectModel
