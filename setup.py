36cf8cc0a14434c52e5293ee1182eb895e3e1130
ovdien <Nickolay.Ovdienko@emc.com>
2017-11-09 17:24:58 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ObjectModeler',
+    version='1.0',
+    packages=['object_modeler'],
+    url='https://github.com/KeyStorke/ObjectModeler',
+    license='MIT',
+    author='Nickolay Ovdienko',
+    author_email='Nickolay.Ovdienko@emc.com',
+    description='Easy and simply define objects schemes in applications'
+)
