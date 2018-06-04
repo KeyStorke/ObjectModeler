f834cb4a1de0a267e9480e943a6a0da2093739c0
Nickolay Ovdienko <info@malware.one>
2018-06-04 23:14:57 +0300

    Revert "release 1.0"
    This reverts commit bb435d7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     test_suite='test',
     name='object_modeler',
-    version='1.0',
+    version='0.0.12',
     packages=['object_modeler'],
     url='https://github.com/KeyStorke/ObjectModeler',
     license='MIT',
