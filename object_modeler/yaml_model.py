808a4a291bcf9ecc50fbe51efc7ba47b22f34ba0
Nickolay Ovdienko <info@malware.one>
2018-07-30 01:38:08 +0300

    draft of yaml models implementation

---
+++ object_modeler/yaml_model.py
@@ -0,0 +1,135 @@
+"""
+It's just draft.
+I want to implement YAML-based models and just have experimented on that idea and tried think up realization
+"""
+
+
+import yaml
+import json
+from object_modeler import SlotsObjectModel
+from object_modeler.common import Undefined, Field
+import importlib
+
+
+TYPES = {
+    'dictionary': dict,
+    'string': str,
+    'integer': int,
+    'boolean': bool,
+    'null': None,
+    'list': list,
+    'unicode': str
+}
+
+SERIALIZERS = dict()
+VALUES = dict()
+
+
+with open('../test_model.yml', 'r') as hfile:
+    data = dict(yaml.safe_load(hfile))
+
+all_fields_names = tuple(field_name for field_name in data['fields'])
+
+required_fields_names = tuple(field_name for field_name, value in data['fields'].items() if 'required' in value)
+optional_fields_names = tuple(field_name for field_name, value in data['fields'].items() if 'optional' in value)
+field_types_as_strings = {field_name: data['definition'][field_name]['type'] for field_name in all_fields_names}
+field_types = dict()
+
+# load defined classes, getting default values and import serializers
+
+for type_name in data['types_definitions']:
+    module = importlib.import_module(data['types_definitions'][type_name])
+    TYPES[type_name] = getattr(module, type_name)
+
+for serializer_name in data['serialisers_definitions']:
+    module = importlib.import_module(data['serialisers_definitions'][serializer_name])
+    SERIALIZERS[serializer_name] = getattr(module, serializer_name)
+
+for value_name in data['values_definitions']:
+    module = importlib.import_module(data['values_definitions'][value_name])
+    VALUES[value_name] = getattr(module, value_name)
+
+default_values = dict()
+hidden_fields = dict()
+serializers_as_string = dict()
+serializers = dict()
+
+for field_name in all_fields_names:
+    hidden_fields[field_name] = data['definition'][field_name].get('hidden', False)
+    default_value = data['definition'][field_name].get('default_value', Undefined())
+
+    if default_value in VALUES:
+        default_values[field_name] = VALUES[default_value]
+    else:
+        default_values[field_name] = default_value
+
+    serializer_name = data['definition'][field_name].get('serializer', None)
+    serializers[field_name] = SERIALIZERS.get(serializer_name, Undefined)
+
+    if isinstance(field_types_as_strings[field_name], list):
+        types = tuple([TYPES[typename] for typename in field_types_as_strings[field_name]])
+    else:
+        types = tuple([TYPES[field_types_as_strings[field_name]]])
+
+    field_types[field_name] = types
+
+# yaml parsed
+
+print(json.dumps(data, indent=True))
+
+print('all_fields_names: {}'.format(all_fields_names))
+print('required_fields_names: {}'.format(required_fields_names))
+print('optional_fields_names: {}'.format(optional_fields_names))
+print('field_types_as_strings: {}'.format(field_types_as_strings))
+print('default_values: {}'.format(default_values))
+print('hidden_fields: {}'.format(hidden_fields))
+print('serializers: {}'.format(serializers))
+print('TYPES: {}'.format(TYPES))
+print('SERIALIZERS: {}'.format(SERIALIZERS))
+print('VALUES: {}'.format(VALUES))
+
+# generate metadata for creation new class
+
+_class_data = {
+    '_all_fields': all_fields_names,
+    '_fields_types': field_types,
+    '_default_values': default_values,
+    '_optional_fields': optional_fields_names,
+    '_hidden_fields': hidden_fields,
+    '_serializers': serializers,
+}
+
+class_data = {}
+
+# generate new data model by generated metadata
+
+for field_name in _class_data['_all_fields']:
+    field_is_optional = field_name in _class_data['_optional_fields']
+    field_is_hidden = _class_data['_hidden_fields'][field_name]
+    field_default_val = _class_data['_default_values'].get(field_name, Undefined())
+    field_serializer = _class_data['_serializers'].get(field_name, Undefined())
+    field_types = _class_data['_fields_types'][field_name]
+
+    field = Field(
+        types=field_types,
+        optional=field_is_optional,
+        default_value=field_default_val,
+        hidden=field_is_hidden,
+        serializer=field_serializer
+    )
+
+    class_data[field_name] = field
+
+
+UserModel = type(data['model_name'], (SlotsObjectModel,), class_data)
+
+model = UserModel(data={
+    "id": 123,
+    "_id": 321,
+})
+
+print(UserModel)
+print(dir(UserModel))
+print(model)
+print(dir(model))
+print(model.to_dict())
