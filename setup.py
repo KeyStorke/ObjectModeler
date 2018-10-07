19304b35d69da035e0e26d1fcdecdc3608e44a54
Nickolay Ovdienko <info@malware.one>
2018-10-07 12:53:28 +0300

    added pyYAML to requirements on setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(
     name='object_modeler',
     version='1.0.1',
     packages=['object_modeler'],
+    install_requires=[
+        'pyyaml'
+    ],
     url='https://github.com/KeyStorke/ObjectModeler',
     license='MIT',
     author='Nickolay Ovdienko',
